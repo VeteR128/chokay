034de055ec7c783bd4bb02aaab7b911d680dfe0d
MSI <serejaskok@mail.ru>
2023-03-21 18:28:17 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,46 @@
+const menuButton = document.querySelector(".header__icons_button");
+const popup = document.querySelectorAll(".popup");
+const closeButton = document.querySelector(".popup__button_img");
+const body = document.querySelector(".body");
+
+menuButton.onclick = function () {
+  menuButton.classList.toggle("active");
+};
+function openPopup(item) {
+  body.classList.add("body_active");
+  item.classList.add("popup_active");
+  document.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      closePopup(item);
+    }
+  });
+}
+function closePopup(item) {
+  item.classList.remove("popup_active");
+  body.classList.remove("body_active");
+  menuButton.classList.remove("active");
+  document.removeEventListener("keydown", (evt) => {
+    if (evt.key === "Escape" && menuButton.classList.contains("active")) {
+      closePopup(item);
+    }
+  });
+}
+popup.forEach((item) => {
+  document.addEventListener("click", (evt) => {
+    if (
+      (evt.target === menuButton ||
+        evt.target.classList.contains("header__icons_toggle")) &&
+      !body.classList.contains("body_active")
+    ) {
+      openPopup(item);
+    }
+    if (
+      (evt.target === menuButton ||
+        evt.target.classList.contains("header__icons_toggle")) &&
+      !menuButton.classList.contains("active")
+    ) {
+      console.log(menuButton.classList.contains("active"));
+      closePopup(item);
+    }
+  });
+});
